48b20b0888a587f6aad275fbd837eb4eb8268dad
Jatin Singh <t-jatin3.singh@ocltp.com>
2023-04-10 15:02:09 +0530

    31 March 2023

--- src/Visitor/VisitorPatternDemo.java
+++ src/Visitor/VisitorPatternDemo.java
@@ -5,6 +5,17 @@ package Visitor;
 public class VisitorPatternDemo {
     public static void main(String[] args) {
 
+        // it will only print keyboard
+        ComputerPart keyboard = new Keyboard();
+        keyboard.accept(new ComputerPartDisplayVisitor());
+        // it will only print monitor
+        ComputerPart monitor = new Monitor();
+        monitor.accept(new ComputerPartDisplayVisitor());
+        // it will only print mouse
+        ComputerPart mouse = new Mouse();
+        mouse.accept(new ComputerPartDisplayVisitor());
+
+        // This will print all computer parts
         ComputerPart computer = new Computer();
         computer.accept(new ComputerPartDisplayVisitor());
     }
